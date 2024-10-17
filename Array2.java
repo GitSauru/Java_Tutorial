b3c336b35259eae557937a94dc4107eb12fc4571
Saurabh <saurabhkale.1497@gmail.com>
2024-10-17 23:26:16 +0530

    Array2 changed

--- Array2.java
+++ Array2.java
@@ -1,6 +1,6 @@
 public class Array2 {
     public static void main(String[] args) {
-     int [] numbers ={1,2,3,4,5};
+     int [] numbers ={1,2,3,4,5,6};
        int a= 0;
        for(int i=0; i<numbers.length; i++){
         a +=numbers[i];
